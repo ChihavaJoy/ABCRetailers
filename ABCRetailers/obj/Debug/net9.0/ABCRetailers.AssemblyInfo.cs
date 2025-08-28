e23a29864cfac61a8271bc027712084f1b9d28e5
Joy <166289564+ChihavaJoy@users.noreply.github.com>
2025-08-28 23:20:29 +0200

    commit

--- ABCRetailers/obj/Debug/net9.0/ABCRetailers.AssemblyInfo.cs
+++ ABCRetailers/obj/Debug/net9.0/ABCRetailers.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ABCRetailers")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2cb120c0d65e057ede847b60813e2f4c38223f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ABCRetailers")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ABCRetailers")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
